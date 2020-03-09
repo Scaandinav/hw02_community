66aa1e507b69c430875629197fe452c93cafb09e
f.belokurov <skandinavian31@yandex.ru>
2020-03-09 15:14:10 +0300

    ..

--- posts/admin.py
+++ posts/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from .models import Post, Group
 
 
-
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("pk", "text", "pub_date", "author", "group")
+    list_display = ("pk", "text", "pub_date", "author")
     search_fields = ("text",)
     list_filter = ("pub_date",)
     empty_value_display = '-пусто-'
@@ -14,8 +13,8 @@ admin.site.register(Post, PostAdmin)
 
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ("title", "slug", "description")
-    search_fields = ("text",)
+    list_display = ("pk", "title", "description")
+    list_filter = ("title",)
     empty_value_display = '-пусто-'
 
 
--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
 from posts.models import Post, Group
 
 
 def index(request):
     latest = Post.objects.order_by("-pub_date")[:11]
+    output = []
+    for item in latest:
+        output.append(item.text)
     return render(request, "index.html", {"posts": latest})
-
 
 
 def group_posts(request, slug):
